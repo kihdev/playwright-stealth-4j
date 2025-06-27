15b71e381abf46ea47a38a356dc232b890cc29e6
Fabrizio Fortino <fabrizio.fortino@gmail.com>
2025-06-27 22:27:12 +0200

    bump playwright to v1.53

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 group = "io.github.kihdev"
 version = "1.1.2-SNAPSHOT"
 
-val playwrightVersion = "1.51.0"
+val playwrightVersion = "1.53.0"
 object Meta {
     const val NAME = "playwright-stealth-4j"
     const val DESC = "A Kotlin-based library to enhance Playwright's stealth capabilities for Java, Kotlin, and Groovy."
